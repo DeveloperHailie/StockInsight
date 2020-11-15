d9eaaa6733588b23482fd7bc05539cc3bfb294a1
hailie <59546976+DeveloperHailie@users.noreply.github.com>
2020-11-15 20:44:58 +0900

    서블릿

--- project_eclip/src/Controller/WriteQuestion.java
+++ project_eclip/src/Controller/WriteQuestion.java
@@ -3,8 +3,6 @@ package Controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -35,50 +33,39 @@ public class WriteQuestion extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-			request.setCharacterEncoding("UTF-8");
-	      
-	      String title = request.getParameter("title");
-	      String content = request.getParameter("content");
-
-	      ServletContext sc = getServletContext();
-	      Connection conn = (Connection) sc.getAttribute("DBconnection");
-	      
-	      // 로그인 되어 있는 상태인지 체크
-	      HttpSession session = request.getSession(false);
-	      if(session==null) {
-	    	// 로그인 안 되어있으면 login.html 반환
-	    	  response.sendRedirect("login.html");
-	    	  
-	      }else {
-	    	  // 로그인 되어 있으면 (1)question db update, (2)작성된 글 보는 페이지 반환
-	    	  // 사용자 idx 가져오기
-	    	  //int uidx = session.getAttribute(idx);
-	    	  int uidx=1;
-	    	  if(DBUtil.addQuestion(conn, uidx, title, content)) {
-	    		  
-	    	  }
-	      }
-	      
-	      
-	     
-	      
-	      
-	      
-	      //ResultSet rs = DBUtil.findUser(conn, user_id); //id 비교
-
-	      PrintWriter out = response.getWriter();
-	   
-	      /*if(true) {
-	         try
-	         {
-	           
-	         } catch (SQLException e) {
-	            e.printStackTrace();
-	         } 
-	      } */
+		request.setCharacterEncoding("UTF-8");
 		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+
+		ServletContext sc = getServletContext();
+		Connection conn = (Connection) sc.getAttribute("DBconnection");
+
+		// 로그인 되어 있는 상태인지 체크
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			// 로그인 안 되어있으면 login.html 반환
+			response.sendRedirect("login.html");
+
+		} else {
+			// 로그인 되어 있으면 (1)question db update, (2)작성된 글 보는 페이지 반환
+			// 사용자 idx 가져오기
+			// int uidx = session.getAttribute(idx);
+			int uidx = 1;
+			if (DBUtil.addQuestion(conn, uidx, title, content)) {
+
+			}
+		}
+
+		// ResultSet rs = DBUtil.findUser(conn, user_id); //id 비교
+
+		PrintWriter out = response.getWriter();
+
+		/*
+		 * if(true) { try {
+		 * 
+		 * } catch (SQLException e) { e.printStackTrace(); } }
+		 */
 	}
 
 	/**
